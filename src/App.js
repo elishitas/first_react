217b0253f4f09bd96dffa38ab289e5b6eedf7c17
Maria Elisa Araya <arayamariaelisa@gmail.com>
2020-11-23 23:50:02 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
+import logo from './assets/logo_meli.png';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <nav>
+          <img src={logo} alt=""/>
+
+          <div className="search-inputs">
+            <input type="text" placeholder="Buscar productos, marcas y más..."/>
+            <button>Buscar</button>
+          </div>
+
+        </nav>
+
       </header>
     </div>
   );
